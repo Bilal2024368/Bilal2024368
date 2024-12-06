a8509cfd6f36af1c45cdeea3a9637d05c399a37a
Bilal2024368 <BILALAFZALKHAN413@GMAIL.COM>
2024-12-06 14:51:14 +0000

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,10 @@
+num =int(input(Enter a non-negative integer: "))
+               
+
+print("Factorials from 0 to {num}:")
+factorial = 1
+
+for i in range(, num + 1):
+     if i > 0:
+          factorial *= i
+       print(F"{i} ={factorial}")        
