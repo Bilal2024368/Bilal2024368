2b166be0036ca4598d4027a4044451a0d588afd9
Bilal2024368 <BILALAFZALKHAN413@GMAIL.COM>
2024-12-06 14:15:11 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+
+number1 = float(input("Enter the first number:  "))
+number2 = float(input("Enter the second number:  "))
+
+sum_result = number1 + number2
+difference = number1 - number2
+product - number1 + number2
+quotient = number1 / number2
+print(F"The sum of numbers is: {sum_result}")
+print (F"The difference between numbers is: {difference}")
+print (F"The product of numbers is: {product}")
+print (F"The quotient of numbers is:{quotient}")
