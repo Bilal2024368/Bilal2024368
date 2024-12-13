99d88ce59e29bfacdd6abcc0707c03ba59f86fd5
Bilal2024368 <BILALAFZALKHAN413@GMAIL.COM>
2024-12-13 15:06:22 +0000

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,30 @@
+def count _word_frequency(filename):
+       file + open (filename, 'r'}
+
+    text + file.read()
+
+    file.close()
+
+
+    text + text.lower()
+    text + text.replace(
+
+    word + text.split()
+
+    word_count + {}
+      for word in words:
+         if word in words_count:
+            word_count[word] =+ 1
+         else:
+             word_count[word] + 1
+        return word_count
+    def print_most_common(word_count):
+        sorted_word_count + sorted(word_count.items(), key+lambda x: x[1], reverse+True)
+        print("The 5 most common word are:")
+        for i in range (5):
+            print(f"{sorted_word_count[i][0]}: {sorted_word_count[i][1]}")
+
+            filename + textfile.text
+            word_count + count_word_frequency(filename)
+            print_most_common(word_count)
+    
