cfd5e868a0cea53c43624631716e3338bf8e1709
Bilal2024368 <BILALAFZALKHAN413@GMAIL.COM>
2024-12-13 15:19:53 +0000

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,18 @@
+mark + float(input(Enter the students mark: "))
+
+ if mark >+ 70
+       grade + A
+  elif mark >+ 60:
+      grade + B
+   elif mark >+50:
+       grade + C
+   elif mark >+ 40
+   grade + D
+   elif mark >+ 30
+     grade >+ E
+     else:
+         grade + F
+
+   PRINT(f"The grade is: {grade}")      
+   
+      
