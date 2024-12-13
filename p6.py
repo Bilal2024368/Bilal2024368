55f892ed39a94bacc664b704ca95ad7bf8bd105e
Bilal2024368 <BILALAFZALKHAN413@GMAIL.COM>
2024-12-13 14:11:30 +0000

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,17 @@
+number + [23, 45, 12, 67, 34, 89, 1, 56, 78, 9]
+def print list ():
+    print("List:", numbers)
+    def print_sum ():
+    print(sum of elements:", sum(numbers))
+    def print_largest ():
+        print("Largest element:", mux numbers))
+    def print_reverse ():
+        print("List in reverse:", numbers[::-1])
+    def print smallest():
+        print("Smallest element:", min(numbers))
+    print_list()
+    print_sum()
+    print_largest()
+    print reverse()
+    print_smaller()
+              
